1b27967d588912d87fec9f77e410b1595d8786d6
Vinitha Natarajan <vinitha@rabidtech.co.nz>
2017-07-26 16:32:47 +1200

    print list of numbers using linked list.

---
+++ c-exercises/structs/linked-list.c
@@ -0,0 +1,55 @@
+// make a linked list that has one member
+// then make 5 nodes - with each node having 1,2,3,4,5 as data
+// then print them out
+// then work out how to reverse the list by only changing the pointers
+// then print again
+
+#include <stdio.h>
+#include <stdlib.h> // Including this header to use malloc
+
+struct node {
+  int num;
+  struct node *next;
+};
+
+struct node *head = NULL;
+struct node *p = NULL;
+
+void insert(int num) {
+  struct node *point = (struct node*) malloc(sizeof(struct node));
+
+  point->num = num;
+  point->next = NULL;
+
+  if(head==NULL) {
+    head = point;
+    head->next = point;
+    return;
+  }
+  p = head;
+  while(p->next != head)
+    p = p->next;
+  p->next = point;
+  point->next = head;
+}
+void printNum() {
+  struct node *pntr = head;
+
+  printf("\nhead:");
+
+  while(pntr->next != head) {
+    printf(" %d ", pntr->num);
+    pntr = pntr->next;
+  }
+  printf(" %d ", pntr->num);
+  printf("\n");
+}
+int main() {
+  insert(1);
+  insert(2);
+  insert(3);
+  insert(4);
+  insert(5);
+
+  printNum();
+}
